98ef98858e7b4c064d51d391e9c8de5913f5bb9b
v138468 <v138468@gmail.com>
2025-06-18 09:10:12 +0900

    gen code test

---
+++ DismantleDoll/Assets/Scripts/Tests/GameRoot.Designer.cs
@@ -0,0 +1,13 @@
+// Generate Id:b2be67ab-a23f-4cd9-a9eb-2682436d3e8c
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFramework.TestApp
+{
+	public partial class GameRoot
+	{
+		public Transform Player;
+		
+	}
+}
